e43eed839343a606e43a812554bef6904f83a01a
lnarolski <narolskilukasz+github@gmail.com>
2021-12-02 15:59:04 +0100

    Created solution for MVS, Added .gitignore

---
+++ 1/Star 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Star_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Star 1!");
+        }
+    }
+}
---
+++ 1/Star 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Star_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Star 2!");
+        }
+    }
+}
