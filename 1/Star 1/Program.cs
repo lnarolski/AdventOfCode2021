ce64837618eb5d9936ecbd2d752713fdf95ece39
lnarolski <narolskilukasz+github@gmail.com>
2021-12-02 18:14:07 +0100

    Finished Day 1

--- 1/Star 1/Program.cs
+++ 1/Star 1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Star_1
 {
@@ -7,6 +9,21 @@ namespace Star_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Star 1!");
+
+            var fileLines = File.ReadLines("../../../../input.txt");
+            string[] fileLinesArray = fileLines.ToArray();
+
+            int numOfLargerMeas = 0;
+
+            for (int i = 1; i < fileLinesArray.Length; ++i)
+            {
+                if (Convert.ToInt32(fileLinesArray[i]) > Convert.ToInt32(fileLinesArray[i - 1]))
+                {
+                    ++numOfLargerMeas;
+                }
+            }
+
+            Console.WriteLine("numOfLargerMeas: {0}", numOfLargerMeas);
         }
     }
 }
--- 1/Star 2/Program.cs
+++ 1/Star 2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Star_2
 {
@@ -7,6 +9,21 @@ namespace Star_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Star 2!");
+
+            var fileLines = File.ReadLines("../../../../input.txt");
+            int[] fileLinesArray = fileLines.ToArray().Select(s => int.Parse(s)).ToArray();
+
+            int numOfLargerMeas = 0;
+
+            for (int i = 0; i < fileLinesArray.Length - 3; ++i)
+            {
+                if (fileLinesArray[i] + fileLinesArray[i + 1] + fileLinesArray[i + 2] < fileLinesArray[i + 1] + fileLinesArray[i + 2] + fileLinesArray[i + 3])
+                {
+                    ++numOfLargerMeas;
+                }
+            }
+
+            Console.WriteLine("numOfLargerMeas: {0}", numOfLargerMeas);
         }
     }
 }
