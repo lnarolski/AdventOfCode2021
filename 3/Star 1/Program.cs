0bd9d7ee71d360c37736a0f690d3e3beeb01361a
lnarolski <narolskilukasz+github@gmail.com>
2021-12-03 17:40:24 +0100

    Working on Day 3

---
+++ 3/Star 1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Star_1
+{
+    class Command
+    {
+        private string command { get; set; }
+        private int value { get; set; }
+
+        public string CommandGet() { return this.command; }
+        public int ValueGet() { return this.value; }
+
+        public Command(string command, int value)
+        {
+            this.command = command;
+            this.value = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Star 1!");
+
+            var fileLines = File.ReadLines("../../../../input.txt");
+            List<string> diagnosticReport = new List<string>();
+
+            foreach (var item in fileLines)
+            {
+                diagnosticReport.Add(item);
+            }
+
+            string epsilonRateString = string.Empty, gammaRateString = string.Empty;
+            int epsilonRate, gammaRate;
+
+            for (int i = 0; i < diagnosticReport[0].Length; ++i)
+            {
+                int numOf0s = 0, numOf1s = 0;
+
+                for (int j = 0; j < diagnosticReport.Count; ++j)
+                {
+                    if (diagnosticReport[j][i] == '1')
+                    {
+                        ++numOf1s;
+                    }
+                    else
+                    {
+                        ++numOf0s;
+                    }
+                }
+
+                if (numOf1s > numOf0s)
+                {
+                    epsilonRateString += '1';
+                    gammaRateString += '0';
+                }
+                else
+                {
+                    epsilonRateString += '0';
+                    gammaRateString += '1';
+                }
+
+            }
+
+            epsilonRate = Convert.ToInt32(epsilonRateString, 2);
+            gammaRate = Convert.ToInt32(gammaRateString, 2);
+
+            Console.WriteLine("Output: {0}", epsilonRate * gammaRate);
+        }
+    }
+}
---
+++ 3/Star 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Star_2
+{
+    class Command
+    {
+        private string command { get; set; }
+        private int value { get; set; }
+
+        public string CommandGet() { return this.command; }
+        public int ValueGet() { return this.value; }
+
+        public Command(string command, int value)
+        {
+            this.command = command;
+            this.value = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Star 2!");
+
+            var fileLines = File.ReadLines("../../../../input.txt");
+            List<Command> commands = new List<Command>();
+
+            foreach (var item in fileLines)
+            {
+                commands.Add(new Command(item.Split(' ')[0], int.Parse(item.Split(' ')[1])));
+            }
+
+            int[] position = { 0, 0, 0 }; // x,y,aim
+
+            foreach (var item in commands)
+            {
+                switch (item.CommandGet())
+                {
+                    case "forward":
+                        position[0] += item.ValueGet();
+                        position[1] += position[2] * item.ValueGet();
+                        break;
+                    case "down":
+                        position[2] += item.ValueGet();
+                        break;
+                    case "up":
+                        position[2] -= item.ValueGet();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("position: {0},{1} aim:{3} Multiplied: {2}", position[0].ToString(), position[1].ToString(), (position[0] * position[1]).ToString(), position[2]);
+        }
+    }
+}
