113dc3e2259230e48a744c4efc1d478bcb41d220
lnarolski <narolskilukasz+github@gmail.com>
2021-12-05 13:53:27 +0100

    Working on Day 5

---
+++ 5/Star 1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Star_1
+{
+    class Line
+    {
+        public int x1, y1;
+        public int x2, y2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Star 1!");
+
+            var fileLines = File.ReadLines("../../../../input.txt");
+            List<string> input = new List<string>();
+
+            foreach (var item in fileLines)
+            {
+                input.Add(item);
+            }
+
+            // Parsing input data
+            int maxX = 1000, maxY = 1000;
+            List<Line> lines = new List<Line>();
+
+            foreach (var item in input)
+            {
+                var coordinates = item.Split(" -> ");
+                lines.Add(new Line()
+                {
+                    x1 = int.Parse(coordinates[0].Split(',')[0]),
+                    y1 = int.Parse(coordinates[0].Split(',')[1]),
+                    x2 = int.Parse(coordinates[1].Split(',')[0]),
+                    y2 = int.Parse(coordinates[1].Split(',')[1]),
+                });
+            }
+
+            int[,] map = new int[maxY + 1, maxX + 1];
+            //
+
+            // Searching for an answer
+            foreach (var item in lines)
+            {
+                if (item.x1 == item.x2 || item.y1 == item.y2)
+                {
+                    int distance = (int)Math.Sqrt(Math.Pow(item.x2 - item.x1, 2) + Math.Pow(item.y2 - item.y1, 2));
+                    for (int i = 0; i < distance + 1; i++)
+                    {
+                        int x;
+                        int y;
+
+                        if (item.x1 < item.x2)
+                        {
+                            double a = (item.y2 - item.y1) / (item.x2 - item.x1);
+                            double b = item.y1 - a * item.x1;
+
+                            x = item.x1 + i;
+                            y = (int)(a * (item.x1 + i) + b);
+                        }
+                        else if (item.x1 > item.x2)
+                        {
+                            double a = (item.y2 - item.y1) / (item.x2 - item.x1);
+                            double b = item.y1 - a * item.x1;
+
+                            x = item.x2 + i;
+                            y = (int)(a * (item.x2 + i) + b);
+                        }
+                        else
+                        {
+                            if (item.y1 < item.y2)
+                            {
+                                x = item.x1;
+                                y = item.y1 + i;
+                            }
+                            else
+                            {
+                                x = item.x1;
+                                y = item.y2 + i;
+                            }
+                        }
+
+                        ++map[y, x];
+                    }
+                }
+            }
+
+            int foundIntersections = 0;
+            for (int y = 0; y < maxY; y++)
+            {
+                for (int x = 0; x < maxX; x++)
+                {
+                    if (map[y,x] > 1)
+                    {
+                        ++foundIntersections;
+                    }
+                }
+            }
+            //
+
+            Console.WriteLine("Output: {0}", foundIntersections);
+        }
+    }
+}
---
+++ 5/Star 2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Star_2
+{
+    class BingoNumber
+    {
+        public int bingoNumber;
+        public bool marked = false;
+
+        public BingoNumber(int bingoNumber)
+        {
+            this.bingoNumber = bingoNumber;
+        }
+    }
+    class BingoLine
+    {
+        public List<BingoNumber> bingoLine;
+        public int numOfFoundNumbersRow = 0;
+        public List<int> numOfFoundNumbersColumn = new List<int> {0,0,0,0,0};
+
+        public BingoLine(int[] bingoLine)
+        {
+            this.bingoLine = new List<BingoNumber>();
+            foreach (var item in bingoLine)
+            {
+                this.bingoLine.Add(new BingoNumber(item));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Star 2!");
+
+            var fileLines = File.ReadLines("../../../../input.txt");
+            List<string> input = new List<string>();
+
+            foreach (var item in fileLines)
+            {
+                input.Add(item);
+            }
+
+            // Parsing input data
+            int[] generatedNumbers = input[0].Split(',').Select(s => int.Parse(s)).ToArray();
+            List<List<BingoLine>> boards = new List<List<BingoLine>>();
+
+            boards.Add(new List<BingoLine>());
+            int inputLine = 2;
+            while (inputLine < input.Count)
+            {
+                if (input[inputLine] == "")
+                {
+                    boards.Add(new List<BingoLine>());
+                }
+                else
+                {
+                    boards[boards.Count - 1].Add(new BingoLine(input[inputLine].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray()));
+                }
+
+                ++inputLine;
+            }
+            //
+
+            // Searching for an answer
+            bool found = false;
+            int sumOfUnmarkedNumbers = 0, numberThatWasJustCalled = -1, winningBoard = -1, wonBoards = 0;
+            List<int> ignoreBoards = new List<int>();
+
+            for (int l = 0; wonBoards != boards.Count && l < generatedNumbers.Length; l++)
+            {
+                for (int i = 0; i < boards.Count; i++)
+                {
+                    for (int j = 0; !ignoreBoards.Contains(i) && j < boards[i].Count; j++) // rows
+                    {
+                        for (int k = 0; k < boards[i][j].bingoLine.Count; k++)
+                        {
+                            if (generatedNumbers[l] == boards[i][j].bingoLine[k].bingoNumber)
+                            {
+                                boards[i][j].bingoLine[k].marked = true;
+                                ++boards[i][j].numOfFoundNumbersRow;
+                                ++boards[i][0].numOfFoundNumbersColumn[k];
+
+                                break;
+                            }
+                        }
+
+                        if (boards[i][j].numOfFoundNumbersRow == boards[i][j].bingoLine.Count || boards[i][0].numOfFoundNumbersColumn.Contains(boards[i].Count))
+                        {
+                            found = true;
+                        }
+                    }
+
+                    if (found)
+                    {
+                        winningBoard = i;
+                        ignoreBoards.Add(i);
+
+                        numberThatWasJustCalled = generatedNumbers[l];
+                        ++wonBoards;
+
+                        found = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < boards[winningBoard].Count; ++i)
+            {
+                for (int j = 0; j < boards[winningBoard][i].bingoLine.Count; ++j)
+                {
+                    if (boards[winningBoard][i].bingoLine[j].marked == false)
+                        sumOfUnmarkedNumbers += boards[winningBoard][i].bingoLine[j].bingoNumber;
+                }
+            }
+            //
+
+            Console.WriteLine("Output: {0}", sumOfUnmarkedNumbers * numberThatWasJustCalled);
+        }
+    }
+}
