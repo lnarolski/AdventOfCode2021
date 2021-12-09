634dd885d94e82e93201faa0a8e5cc03212dd00e
lnarolski <narolskilukasz+github@gmail.com>
2021-12-09 14:10:25 +0100

    Working on Day 9

--- 9/Star 1/Program.cs
+++ 9/Star 1/Program.cs
@@ -7,6 +7,20 @@ namespace Star_1
 {
     class Program
     {
+        static private bool IsLowpoint(int x, int y, List<List<long>> map)
+        {
+            if (x != 0 && map[y][x] >= map[y][x - 1]) // left
+                return false;
+            if (x != map[0].Count - 1 && map[y][x] >= map[y][x + 1]) //right
+                return false;
+            if (y != 0 && map[y][x] >= map[y - 1][x]) //up
+                return false;
+            if (y != map.Count - 1 && map[y][x] >= map[y + 1][x]) //down
+                return false;
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Star 1!");
@@ -20,14 +34,28 @@ namespace Star_1
             }
 
             // Parsing input data
-
+            List<List<long>> map = new List<List<long>>();
+            foreach (var line in fileLines)
+            {
+                map.Add(line.ToCharArray().Select(s => long.Parse(s.ToString())).ToList());
+            }
             //
 
             // Searching for an answer
-
+            long risk = 0;
+            for (int y = 0; y < map.Count; y++)
+            {
+                for (int x = 0; x < map[0].Count; x++)
+                {
+                    if (IsLowpoint(x, y, map))
+                    {
+                        risk += map[y][x] + 1;
+                    }
+                }
+            }
             //
 
-            Console.WriteLine("Output: {0}");
+            Console.WriteLine("Output: {0}", risk);
         }
     }
 }
