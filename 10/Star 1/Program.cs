9f6b04c64a2ee563f2a7639f06d448798a9569a9
lnarolski <narolskilukasz+github@gmail.com>
2021-12-10 11:20:26 +0100

    Working on Day 10

--- 10/Star 1/Program.cs
+++ 10/Star 1/Program.cs
@@ -34,14 +34,44 @@ namespace Star_1
             }
 
             // Parsing input data
-
+            Dictionary<char, long> points = new Dictionary<char, long>()
+            {
+                {')', 3}, {']', 57}, {'}', 1197}, {'>', 25137}
+            };
+            Dictionary<char, char> chunks = new Dictionary<char, char>()
+            {
+                {'(', ')'}, {'[', ']'}, {'{', '}'}, {'<', '>'}
+            };
             //
 
             // Searching for an answer
-
+            long syntaxErrorScore = 0;
+            foreach (var line in fileLines)
+            {
+                Stack<char> lastChunk = new Stack<char>();
+                foreach (var character in line)
+                {
+                    if (chunks.ContainsKey(character))
+                    {
+                        lastChunk.Push(character);
+                    }
+                    else
+                    {
+                        if (character != chunks[lastChunk.Peek()])
+                        {
+                            syntaxErrorScore += points[character];
+                            break;
+                        }
+                        else
+                        {
+                            lastChunk.Pop();
+                        }
+                    }
+                }
+            }
             //
 
-            Console.WriteLine("Output: {0}");
+            Console.WriteLine("Output: {0}", syntaxErrorScore);
         }
     }
 }
