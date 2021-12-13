ff76e2c07b14e5c6a77b8c4b422bc557b1732f17
lnarolski <narolskilukasz+github@gmail.com>
2021-12-13 12:21:19 +0100

    Working on Day 13

--- 13/Star 1/Program.cs
+++ 13/Star 1/Program.cs
@@ -20,14 +20,107 @@ namespace Star_1
             }
 
             // Parsing input data
-            
+            int maxX = int.MinValue, maxY = int.MinValue;
+            List<List<char>> map = new List<List<char>>();
+            List<List<int>> dots = new List<List<int>>();
+            bool folds = false;
+            foreach (var line in fileLines)
+            {
+                if (line == "")
+                {
+                    folds = true;
+
+                    for (int y = 0; y < maxY + 1; y++)
+                    {
+                        map.Add(new List<char>());
+                        for (int x = 0; x < maxX + 1; x++)
+                        {
+                            map[map.Count - 1].Add('.');
+                        }
+                    }
+
+                    foreach (var dot in dots)
+                    {
+                        map[dot[1]][dot[0]] = '#';
+                    }
+
+                    continue;
+                }
+
+                if (!folds)
+                {
+                    int[] temp = line.Split(',').Select(s => int.Parse(s)).ToArray();
+                    if (temp[0] > maxX)
+                        maxX = temp[0];
+                    if (temp[1] > maxY)
+                        maxY = temp[1];
+
+                    dots.Add(temp.ToList());
+                }
+                else
+                {
+                    string[] temp = line.Split("fold along ")[1].Split('=');
+                    int value = int.Parse(temp[1]);
+
+                    if (temp[0] == "x")
+                    {
+                        for (int y = 0; y < map.Count; y++)
+                        {
+                            for (int x = value + 1; x < map[0].Count; x++)
+                            {
+                                if (map[y][x] == '#')
+                                {
+                                    map[y][value - (x - value)] = '#';
+                                    map[y][x] = '.';
+                                }
+                            }
+                        }
+
+                        for (int y = 0; y < map.Count; y++)
+                        {
+                            map[y][value] = '.';
+                        }
+                    }
+                    else // then y
+                    {
+                        for (int y = value + 1; y < map.Count; y++)
+                        {
+                            for (int x = 0; x < map[0].Count; x++)
+                            {
+                                if (map[y][x] == '#')
+                                {
+                                    map[value - (y - value)][x] = '#';
+                                    map[y][x] = '.';
+                                }
+                            }
+                        }
+
+                        for (int x = 0; x < map[value].Count; x++)
+                        {
+                            map[value][x] = '.';
+                        }
+                    }
+
+                    break;
+                }
+            }
             //
 
             // Searching for an answer
-            
+            long visibleDots = 0;
+            for (int y = 0; y < map.Count; y++)
+            {
+                for (int x = 0; x < map[0].Count; x++)
+                {
+                    if (map[y][x] == '#')
+                    {
+                        ++visibleDots;
+                    }
+                }
+            }
             //
 
-            Console.WriteLine("Output: {0}");
+            Console.WriteLine("Output: {0}", visibleDots);
         }
     }
 }
