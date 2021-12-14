f0fb9ca902c3f32cd8214bf32e7fb4c818074c21
lnarolski <narolskilukasz+github@gmail.com>
2021-12-14 22:09:12 +0100

    Working on Day 14

--- 14/Star 2/Program.cs
+++ 14/Star 2/Program.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Star_2
+namespace Star_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from Star 2!");
+            Console.WriteLine("Hello World from Star 1!");
 
             var fileLines = File.ReadLines("../../../../input.txt");
             List<string> input = new List<string>();
@@ -20,7 +20,7 @@ namespace Star_2
             }
 
             // Parsing input data
-            LinkedList<char> polymerTemplate = new LinkedList<char>();
+            Dictionary<string, int> polymerTemplate = new Dictionary<string, int>();
             Dictionary<string, char> pairInsertionRules = new Dictionary<string, char>();
             bool rules = false;
             foreach (var line in fileLines)
@@ -34,7 +34,17 @@ namespace Star_2
 
                 if (!rules)
                 {
-                    polymerTemplate = new LinkedList<char>(line);
+                    for (int i = 0; i < line.Length - 1; i++)
+                    {
+                        if (!polymerTemplate.ContainsKey(line.Substring(i, 2)))
+                        {
+                            polymerTemplate.Add(line.Substring(i, 2), 1);
+                        }
+                        else
+                        {
+                            ++polymerTemplate[line.Substring(i, 2)];
+                        }
+                    }
                 }
                 else
                 {
@@ -48,49 +58,56 @@ namespace Star_2
             // Searching for an answer
             DateTime dateTime = DateTime.Now;
 
-            bool stop;
-            int maxSteps = 40;
+            int maxSteps = 10;
             for (int i = 0; i < maxSteps; i++)
             {
-                LinkedListNode<char> linkedListNode = polymerTemplate.First;
+                Dictionary<string, int> polymerTemplateCopy = new Dictionary<string, int>(polymerTemplate);
 
-                for (int j = 0; j < polymerTemplate.Count - 1; j++)
+                foreach (var polymer in polymerTemplate)
                 {
-                    string polymerTemplatePart = "";
-
-                    polymerTemplatePart += linkedListNode.Value;
-                    linkedListNode = linkedListNode.Next;
-                    polymerTemplatePart += linkedListNode.Value;
-
-                    if (pairInsertionRules.ContainsKey(polymerTemplatePart))
+                    if (pairInsertionRules.ContainsKey(polymer.Key))
                     {
-                        polymerTemplate.AddBefore(linkedListNode, pairInsertionRules[polymerTemplatePart]);
+                        if (!polymerTemplateCopy.ContainsKey(polymer.Key[0].ToString() + pairInsertionRules[polymer.Key]))
+                        {
+                            polymerTemplateCopy.Add(polymer.Key[0].ToString() + pairInsertionRules[polymer.Key], 1);
+                        }
+                        else
+                        {
+                            polymerTemplateCopy[polymer.Key[0].ToString() + pairInsertionRules[polymer.Key]] += polymer.Value;
+                        }
 
-                        j += 1;
+                        if (!polymerTemplateCopy.ContainsKey(pairInsertionRules[polymer.Key].ToString() + polymer.Key[1]))
+                        {
+                            polymerTemplateCopy.Add(pairInsertionRules[polymer.Key].ToString() + polymer.Key[1], 1);
+                        }
+                        else
+                        {
+                            polymerTemplateCopy[pairInsertionRules[polymer.Key].ToString() + polymer.Key[1]] += polymer.Value;
+                        }
                     }
                 }
 
-                Console.WriteLine(i);
-                Console.WriteLine("Execution time: {0}", (DateTime.Now - dateTime).TotalSeconds);
-                dateTime = DateTime.Now;
+                polymerTemplate = new Dictionary<string, int>(polymerTemplateCopy);
             }
 
             long minCount = long.MaxValue, maxCount = long.MinValue;
-            char[] polymerTemplateChar = polymerTemplate.ToArray();
             HashSet<char> characters = new HashSet<char>();
-            for (int i = 0; i < polymerTemplate.Count; i++)
+            foreach (var polymer in polymerTemplate)
             {
-                if (!characters.Contains(polymerTemplateChar[i]))
+                if (!characters.Contains(polymer.Key[0]))
                 {
-                    int temp = polymerTemplate.Count(c => c == polymerTemplateChar[i]);
+                    int temp = polymerTemplate.Count(c => c == polymer.Key[0]);
                     if (temp < minCount)
                         minCount = temp;
                     if (temp > maxCount)
                         maxCount = temp;
 
-                    characters.Add(polymerTemplateChar[i]);
+                    characters.Add(polymer);
                 }
             }
+
+            Console.WriteLine("Execution time: {0}", (DateTime.Now - dateTime).TotalMilliseconds);
+
             //
 
             Console.WriteLine("Output: {0}", maxCount - minCount);
